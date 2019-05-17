76b6b7f1e0b697304e2ef069cbb62418e1fcaea3
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-17 10:37:47 +0100

    Create carousel component - contains various methods for setting a class, moving images, and initializing the carousel
    -style and add classes to the images which either hides or shows the image depending on the index

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,108 @@
 class Carousel {
+    constructor(image) {
+        this.image = image;
+        this.imgLength = image.length;
+        this.slide = 0;
+        this.moving = true;
+        this.initCarousel();
+    }
+    setInitialClasses() {
+        // Targets the previous, current, and next images
 
+        this.image[this.imgLength - 1].classList.add('prev');
+        this.image[0].classList.add("active");
+        this.image[1].classList.add("next");
+
+        // Set event listeners on the buttons
+
+        let prev = document.querySelector('.left-button')
+        let next = document.querySelector('.right-button')
+        // console.log(next)
+
+        next.addEventListener('click', () => this.moveNext());
+        prev.addEventListener('click', () => this.movePrev());
+
+    }
+    moveNext() {
+        if (!this.moving) {
+            if (this.slide === this.imgLength - 1) {
+                this.slide = 0
+            } else {
+                this.slide++
+            }
+
+            this.moveCarousel(this.slide);
+        }
+    }
+
+    movePrev() {
+        if (!this.moving) {
+            if (this.slide === 0) {
+                this.slide = this.imgLength - 1
+            } else {
+                this.slide--
+            }
+
+            this.moveCarousel(this.slide);
+        }
+    }
+
+    disableMove() {
+        this.moving = true;
+        setTimeout(() => {
+            this.moving = false
+        }, 500);
+    }
+
+    moveCarousel(slide) {
+        if (!this.moving) {
+            this.disableMove();
+            
+            if (this.imgLength > 3) {
+                let newPrev = slide - 1;
+                let newNext = slide + 1;
+                let oldPrev = slide - 2;
+                let oldNext = slide + 2;
+
+                if (newPrev <= 0) {
+                    oldPrev = this.imgLength - 1
+                } else if (newNext >= this.imgLength - 1) {
+                    oldNext = 0;
+                }
+
+                if (this.slide === 0) {
+                    newPrev = this.imgLength - 1;
+                    oldPrev = this.imgLength - 2;
+                    oldNext = this.slide + 1;
+                } else if (this.slide === this.imgLength - 1) {
+                    newPrev = this.slide - 1
+                    newNext = 0;
+                    oldNext = 1;
+                }
+                this.image[oldPrev].className = 'carousel-photo';
+                this.image[oldNext].className = 'carousel-photo';
+
+                this.image[newPrev].className = 'carousel-photo' + ' prev'
+                this.image[this.slide].className = 'carousel-photo' + ' active'
+                this.image[newNext].className = 'carousel-photo' + ' next'
+
+            }
+
+
+        }
+    }
+
+    initCarousel() {
+        this.setInitialClasses();
+        this.moving = false;
+    }
+
+    
 }
+let carousel = document.querySelectorAll('.carousel > img');
 
-let carousel = document.querySelector();
+new Carousel(carousel);
+console.log(carousel)
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
